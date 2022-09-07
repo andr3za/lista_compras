8c1b0c5389065d3c2a4255c1053b5a70b2e825c1
Andreza Mitterstein Prebianca <102162509+andr3za@users.noreply.github.com>
2022-09-07 19:59:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Lista de compras!\n");
+
+string[] produtos = new string[10] { "Macarrão", "Arroz", "Cebola", "Tomate", "Suco", "Refrigerante", "Maionese", 
+ "Feijão", "Queijo", "Presunto" };
+string[] carrinhoCompras = new string[10];
+
+int contador = 0;
+
+Console.WriteLine("Digite S para os produtos que você quer e N para os que você não quer!\n");
+
+for (int i = 0; i < produtos.Length; i++)
+{
+    Console.WriteLine(produtos[i]);
+    carrinhoCompras[i] = Console.ReadLine();
+
+    if (carrinhoCompras[i] == "S")
+    {
+        contador = contador + 1;
+    }
+}
+
+if (contador > 0)
+{
+    Console.WriteLine("\nLista de compras:\n");
+
+    for (int i = 0; i < produtos.Length; i++)
+    {
+        if (carrinhoCompras[i] == "S")
+        {
+            Console.WriteLine(produtos[i]);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Você não escolheu nenhum produto!");
+}
